871c31ad17ea4929aa1f2bdb3da792d9a219509d
msd1001` <madhureshsingh1001@gmail.com`>
2022-11-27 19:02:39 +0530

    last commit

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -1,7 +1,20 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import Sinon, { stub } from 'sinon';
 import '../src/Customer/Customer-details.js';
+
+const el = await fixture(html`<customer-details></customer-details>`);
 
 describe('customer details', () => {
   // Write test cases inside this block
+  it('check for accessibility',()=>{
+    expect(el).to.be.accessible;
+  });
+
+  it('spy toEmidetails method', ()=>{
+    const func = Sinon.stub(el, '_toEmidetails');
+    el.shadowRoot.querySelector('lion-button').click()
+    expect(func.calledOnce).to.be.false;
+  })
+
+  
 });
